1a2b876eff72e1777641ca0a72dc27944a39ccbd
ts-oogie <onagususa@gmail.com>
2023-03-06 16:45:00 -0500

    added click functionality to add button in inputMedia component

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -6,35 +6,54 @@ const ProgressBar = ({file}) => {
 
     const {url, progress} = useStorage(file) 
 
-    const [status, setStatus] = useState('')   
+    const [status, setStatus] = useState('')
+    
+    const setPlus = (progress) => {
+         let inputArr = []
+         for(let i=1; i<=progress; i++){
+            inputArr.push("+")
+            let joinStr = inputArr.join('')
+            setStatus(joinStr) 
+         }
+    }
+
+    const alertMe = () => {
+        console.log("clicked")
+    }
 
     useEffect(() => {    
         if(progress==1){
-            setStatus("+")
+            //setStatus("+")
+            setPlus(progress)
         }
         else if(progress == 2){
-            setStatus("++") 
+            //setStatus("++") 
+            setPlus(progress)
         }
         else if(progress == 3){
-            setStatus("+++") 
+            //setStatus("+++") 
+            setPlus(progress)
         }
         else if(progress == 4){
-            setStatus("++++") 
+            //setStatus("++++") 
+            setPlus(progress)
         }
         else if(progress == 5){
-            setStatus("+++++") 
+            //setStatus("+++++") 
+            setPlus(progress)
         }
         else if(progress == 6){
-            setStatus("++++++") 
+            //setStatus("++++++") 
+            setPlus(progress)
         } 
         else if(url){
-            setStatus("Add")
+            setStatus("Add") 
         }
     }, [progress, url])
 
     return (
         //if progress < 100, then cycle button
-        <div className="progressBar"><h2>{status}</h2></div>
+        <div className="progressBar" onClick={alertMe}><h2>{status}</h2></div>
     )
     
 }
